ea7a5a749ce57103c22882e5bb3caf8056d42a90
Kenji Rikitake <kenji.rikitake@acm.org>
2024-03-21 01:29:47 +0900

    HomeBrew-CMake.sh: use /opt/homebrew (for Apple Silicon)

--- HomeBrew-CMake.sh
+++ HomeBrew-CMake.sh
@@ -1,2 +1,2 @@
 cd build
-cmake .. -DLIBUSB_INCLUDE_DIR=/usr/local/opt/libusb/include/libusb-1.0 -DLIBUSB_LIBRARIES=/usr/local/opt/libusb/lib/libusb-1.0.0.dylib
+cmake .. -DLIBUSB_INCLUDE_DIR=/opt/homebrew/opt/libusb/include/libusb-1.0 -DLIBUSB_LIBRARIES=/opt/homebrew/opt/libusb/lib/libusb-1.0.0.dylib
